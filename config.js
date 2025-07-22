2c41f6319d226ec33f359d0634aaf4302d32bca1
edson20081970 <edsonwilnys@gmail.com>
2025-07-22 08:09:51 -0400

    Update config.js

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ module.exports = {
     ANTI_DELETE: process.env.ANTI_DELETE || "false",
     ANTI_CALL: process.env.ANTI_CALL || "true",
 
-    REJECT_MSG: process.env.REJECT_MSG || "*📞 ᴄαℓℓ ɴσт αℓℓσωє∂ ιɴ тнιѕ ɴᴜмвєʀ уσυ ∂σɴт нανє ᴘєʀмιѕѕισɴ 📵*",
+    REJECT_MSG: process.env.REJECT_MSG || "*📞 no call me i am bussy📵*",
     ANTI_BAD: process.env.ANTI_BAD || "false",                  // Block bad words?
 
     ANTI_LINK_KICK: process.env.ANTILINK_KICK || "false",
